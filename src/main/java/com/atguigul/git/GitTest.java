368f4729eef0fc7230fe99625c9d412ce1e8b92c
ben <59186151+yplhy@users.noreply.github.com>
2021-04-30 11:30:34 +0800

    Update GitTest.java

--- src/main/java/com/atguigul/git/GitTest.java
+++ src/main/java/com/atguigul/git/GitTest.java
@@ -6,6 +6,8 @@ public class GitTest {
         System.out.println("hello1");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("hello4");
+        
 
     }
 }
